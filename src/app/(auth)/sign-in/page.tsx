aee6bc0943159b8b5f92bd2af9bef1842d860225
nathkit <nathkit101@gmail.com>
2024-12-28 12:13:38 +0700

    feat:Add sign-in and sign-up pages

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,8 @@
+"use client"
+
+import { SignInCard } from '@/features/auth/components/sign-in-card';
+
+function SignInPage() {
+  return <SignInCard />
+};
+export default SignInPage;
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,8 @@
+"use client"
+
+import { SignUpCard } from '@/features/auth/components/sign-up-card';
+
+function SignUpPage() {
+  return <SignUpCard />
+};
+export default SignUpPage;
