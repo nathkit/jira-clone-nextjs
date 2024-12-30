a267d1997536c0bf96460519b9b772fa332c9aa4
nathkit <nathkit101@gmail.com>
2024-12-30 08:26:10 +0700

    feat: Handling image upload

--- src/features/workspaces/components/create-workspace-form.tsx
+++ src/features/workspaces/components/create-workspace-form.tsx
@@ -2,16 +2,20 @@
 
 import { z } from "zod";
 
+import Image from "next/image";
+import { useRef } from "react";
+import { ImageIcon } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { DottedSeparator } from "@/components/dotted-separator";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"; 
 
 import { createWorkspaceSchema } from "../schemas";
-import { DottedSeparator } from "@/components/dotted-separator";
 import { useCreateWorkspace } from "../api/use-create-workspace";
 
 interface createWorkspaceFormProps {
@@ -20,6 +24,9 @@ interface createWorkspaceFormProps {
 
 export const CreateWorkspaceForm = ({ onCancel }: createWorkspaceFormProps) =>{
     const { mutate, isPending } = useCreateWorkspace();
+
+    const inputRef = useRef<HTMLInputElement>(null);
+
     const form = useForm<z.infer<typeof createWorkspaceSchema>>({
         resolver: zodResolver(createWorkspaceSchema),
         defaultValues: {
@@ -28,8 +35,25 @@ export const CreateWorkspaceForm = ({ onCancel }: createWorkspaceFormProps) =>{
     });
 
     const onSubmit = (values: z.infer<typeof createWorkspaceSchema>) => {
-        mutate({ json: values });
+        const finalValue = {
+            ...values,
+            image: values.image instanceof File ? values.image : "",
+        };
+
+        mutate({ form: finalValue }, {
+            onSuccess: () => {
+                form.reset();
+                // TODO: Redirect to new workspace
+            }
+        });
     };
+
+    const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files?.[0];
+        if (file) {
+            form.setValue("image", file);
+        }
+    }
 
     return (
         <Card className="w-full h-full border-none shadow-none">
@@ -62,6 +86,60 @@ export const CreateWorkspaceForm = ({ onCancel }: createWorkspaceFormProps) =>{
                                         <FormMessage />
                                     </FormItem>
                                 )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="image"
+                                render={({ field }) => {
+                                    const value = field.value!;
+                                    const imageSrc = value instanceof File ? URL.createObjectURL(value) : value
+                                    return (
+                                        <div className="flex flex-col gap-y-2">
+                                            <div className="flex items-center gap-x-5">
+                                                {value ? (
+                                                    <div className="size-[72px] relative rounded-md overflow-hidden">
+                                                        <Image 
+                                                            alt="Logo"
+                                                            fill
+                                                            className="object-cover"
+                                                            src={imageSrc}
+                                                        />
+                                                    </div>
+                                                ) : (
+                                                    <Avatar className="size-[72px]">
+                                                        <AvatarFallback>
+                                                            <ImageIcon className="ize-[36px] text-neutral-400" />
+                                                        </AvatarFallback>
+                                                    </Avatar>
+                                                )}
+                                                <div className="flex flex-col">
+                                                    <p className="text-sm">Workspace Icon</p>
+                                                    <p className="text-sm text-mute-foreground">
+                                                        JPG, PNG, SVG, JPEG, max 1mb
+                                                    </p>
+                                                    <input 
+                                                        className="hidden"
+                                                        type="file"
+                                                        accept=".jpg, .png, .jpeg, .svg"
+                                                        ref={inputRef}
+                                                        onChange={handleImageChange}
+                                                        disabled={isPending}
+                                                    />
+                                                    <Button
+                                                        type="button"
+                                                        disabled={isPending}
+                                                        variant="teritary"
+                                                        size="xs"
+                                                        className="w-fit mt-2"
+                                                        onClick={() => inputRef.current?.click()}
+                                                    >
+                                                        Upload Image
+                                                    </Button>
+                                                </div>                                                
+                                            </div>
+                                        </div>
+                                    )
+                                }}
                             />
                         </div>
                         <DottedSeparator className="py-7" />
